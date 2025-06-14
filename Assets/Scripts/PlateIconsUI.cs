6a5f628ba43c365eea4de616a61154c9e660d789
alexdolgindoesnotusechatgpt <alexdolginwork@gmail.com>
2025-06-14 13:56:13 -0400

    hour 7

---
+++ Assets/Scripts/PlateIconsUI.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlateIconsUI : MonoBehaviour
+{
+    [SerializeField] private PlateKitchenObject plateKitchenObject;
+    [SerializeField] private Transform iconTemplate;
+
+
+    private void Awake()
+    {
+        iconTemplate.gameObject.SetActive(false);
+    }
+    private void Start()
+    {
+        plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
+    }
+
+    private void PlateKitchenObject_OnIngredientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e)
+    {
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child == iconTemplate) continue;
+            Destroy(child.gameObject);
+        }
+
+        foreach (KitchenObjectSO kitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList())
+        {
+            Transform iconTransform = Instantiate(iconTemplate, this.transform);
+            iconTransform.gameObject.SetActive(true);
+            iconTransform.GetComponent<PlateIconsSingleUI>().SetKitchenObjectSO(kitchenObjectSO);
+            Debug.Log("PlateIconsUI: Added icon for " + kitchenObjectSO.objectName);
+        }
+    }
+}
