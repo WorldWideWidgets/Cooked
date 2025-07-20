0753f82fafc7c16dd9b43cd470d62d06a092a757
alexdolgindoesnotusechatgpt <alexdolginwork@gmail.com>
2025-07-20 15:34:31 -0400

    .

--- Assets/Scripts/UI/GameStartCountdownUI.cs
+++ Assets/Scripts/UI/GameStartCountdownUI.cs
@@ -6,6 +6,12 @@ public class GameStartCountdownUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI countdownText;
 
+    private Animator animator;
+    private int previousCountdownNumber;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
     private void Start()
     {
         GameManager.Instance.OnStateChanged += GameManager_OnStateChanged;
@@ -26,7 +32,15 @@ public class GameStartCountdownUI : MonoBehaviour
 
     private void Update()
     {
+        int countdownNumber = Mathf.CeilToInt(GameManager.Instance.GetCountdownToStartTimer());
         countdownText.text = Mathf.Ceil(GameManager.Instance.GetCountdownToStartTimer()).ToString();
+
+        if (previousCountdownNumber != countdownNumber)
+        {
+            previousCountdownNumber = countdownNumber;
+            animator.SetTrigger("NumberPopup");
+            SoundManager.Instance.PlayCountdownSound();
+        }
     }
 
     private void Show()
